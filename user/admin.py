ac943009605193de1059a94fbdbbc5d032570e4b
Iheanyi Oziegbe <neoconstel@gmail.com>
2023-10-03 11:36:07 +0100

    fix major issue of User model saving password as plain text when saved from the admin panel.

--- user/admin.py
+++ user/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 from .models import User, InvalidAccessToken
 
-# Register your models here.
-admin.site.register(User)
-admin.site.register(InvalidAccessToken)
 
+class UserAdmin(admin.ModelAdmin):
+    '''Control what happens when the User model is saved FROM the admin panel.
+    Use set_password (encrypt) only if user object is newly created or password
+    has been changed. This ensures that the new password (plain text) is stored
+    as a hash, and that the existing password hash doesn't get re-encrypted by 
+    accident -- which would cause an unintended password mismatch.'''
+    def save_model(self, request, obj, form, change):
+        if obj.pk:
+            orig_obj = User.objects.get(pk=obj.pk)
+            if obj.password != orig_obj.password:
+                obj.set_password(obj.password)
+        else:
+            obj.set_password(obj.password)
+        obj.save()
+
+
+# Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(InvalidAccessToken)
--- user/models.py
+++ user/models.py
@@ -77,13 +77,8 @@ class User(AbstractBaseUser, PermissionsMixin):
                 f"user_{int(time.time()) + self.__class__.objects.count()}"
                 f"{random.randint(1000,9999)}")            
 
-        # use set_password to ensure password is encrypted before first save
-        if not self.id:
-            self.set_password(self.password)
-
         new_user = super().save(*args, **kwargs)      
         return new_user
-
 
 class InvalidAccessToken(models.Model):
     token = models.CharField(max_length=1000)
