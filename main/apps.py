f2f709792e854c14ae24eb661e5cdf647db967d9
Iheanyi Oziegbe <neoconstel@gmail.com>
2023-10-12 13:03:07 +0100

    User signal: create artist profile on first save

--- main/apps.py
+++ main/apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
+
+    # added part for execution of signals
+    def ready(self) -> None:
+
+        from . import signals
+
+        return super().ready()
+        
---
+++ main/signals.py
@@ -0,0 +1,42 @@
+# inbuilt django signals
+from django.core.signals import request_finished
+from django.db.models.signals import pre_save, post_save, pre_delete, post_delete
+
+# custom signals
+import django.dispatch
+new_following = django.dispatch.Signal()
+
+# decorator for receiving signals
+from django.dispatch import receiver
+
+# models
+from .models import User, Artist
+from django.contrib.contenttypes.models import ContentType
+
+
+
+
+# ----EXAMPLE----
+# <sender> points to the model to listen to. None by default, meaning it listens to all.
+@receiver(request_finished, sender=None, dispatch_uid='unique-string-to-protect-against-duplicate-signal')
+def my_callback(sender, **kwargs):
+    pass
+    # print(f'\n\n\nEXECUTED SIGNAL:  Request Finished!!!\n\n\n')
+
+
+# -------USER-------
+@receiver(post_save, sender=User, dispatch_uid='user-uid')
+def user_listener(sender, **kwargs):
+    model_instance = kwargs.get('instance')
+    newly_created = kwargs.get('created')
+
+    # only create an artist instance IF this instance is newly created
+    if newly_created:
+        # create artist for this user
+        artist = Artist(
+            user=model_instance
+        )
+        artist.save()
+
+        # print(f'\n\n\nEXECUTED SIGNAL:  artist created\n\n\n')
+        
