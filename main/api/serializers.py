e8c28e73cb216acec108b964a44aa086f42f5574
Iheanyi Oziegbe <neoconstel@gmail.com>
2023-10-22 22:07:51 +0100

    - Artwork: file_url - settings: DOMAIN setting

--- main/api/serializers.py
+++ main/api/serializers.py
@@ -1,14 +1,22 @@
 from rest_framework import serializers
 
 from main.models import Artwork
+from django.conf import settings
 
 
 class ArtworkSerializer(serializers.ModelSerializer):
+
+    # custom serializer field
+    file_url = serializers.SerializerMethodField()
+
+    # custom serializer field method to get property
+    # syntax: get_<custom serializer field name>
+    def get_file_url(self, object):
+        return f"{settings.DOMAIN}/{object.file.resource.url}"
 
     class Meta:
         model = Artwork
         fields = '__all__'
         extra_kwargs = {
-            'artist': {'read_only': True},
-            'file': {'read_only': True},
+            'artist': {'read_only': True}
         }
