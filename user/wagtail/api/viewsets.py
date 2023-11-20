c9922f74819376920f624f35a561c6ba2fd93fa1
Iheanyi Oziegbe <neoconstel@gmail.com>
2023-11-20 20:46:50 +0100

    Custom wagtail API viewsets (pages/images/documents)

--- user/wagtail/api/viewsets.py
+++ user/wagtail/api/viewsets.py
@@ -1,10 +1,27 @@
-from wagtail.api.v2.views import PagesAPIViewSet
+from wagtail.api.v2.views import PagesAPIViewSet \
+    as DefaultPagesAPIViewSet
+from wagtail.images.api.v2.views import ImagesAPIViewSet \
+    as DefaultImagesAPIViewSet
+from wagtail.documents.api.v2.views import DocumentsAPIViewSet \
+    as DefaultDocumentsAPIViewSet
 from wagtail.api.v2.router import WagtailAPIRouter
-from wagtail.images.api.v2.views import ImagesAPIViewSet
-from wagtail.documents.api.v2.views import DocumentsAPIViewSet
+
 
 # Create the router. "wagtailapi" is the URL namespace
 api_router = WagtailAPIRouter('wagtailapi')
+
+
+class PagesAPIViewSet(DefaultPagesAPIViewSet):
+    permission_classes = []
+
+
+class ImagesAPIViewSet(DefaultImagesAPIViewSet):
+    permission_classes = []
+
+
+class DocumentsAPIViewSet(DefaultDocumentsAPIViewSet):
+    permission_classes = []
+
 
 # Add the three endpoints using the "register_endpoint" method.
 # The first parameter is the name of the endpoint (such as pages, images). This
