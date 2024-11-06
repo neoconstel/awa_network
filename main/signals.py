ff53d626b754de02ea7dcc7d55740b267009feae
Iheanyi Oziegbe <neoconstel@gmail.com>
2024-11-06 10:31:02 +0100

    Review model -> signals: delete attached files when a review gets deleted

--- main/signals.py
+++ main/signals.py
@@ -10,7 +10,7 @@ new_following = django.dispatch.Signal()
 from django.dispatch import receiver
 
 # models
-from .models import User, Artist, Artwork, File, Image
+from .models import User, Artist, Artwork, File, Image, Review
 from django.contrib.contenttypes.models import ContentType
 
 
@@ -75,3 +75,27 @@ def image_listener(sender, **kwargs):
     model_instance.resource.delete()
 
     # print(f'\n\n\nEXECUTED SIGNAL:  image and attached resource deleted\n\n\n')
+
+
+# -------Review-------
+@receiver(post_delete, sender=Review, dispatch_uid='review-uid')
+def review_listener(sender, **kwargs):
+    model_instance = kwargs.get('instance')
+    
+    # delete attached file instance (file or image object as per generic field)
+    # model_instance.file.delete()
+    caption_media_object = \
+        model_instance.caption_media_type.model_class().objects.get(
+        id=model_instance.caption_media_id)
+    caption_media_object.delete()
+
+    # delete optionally-attached file instance (file or image object as per generic field)
+    # model_instance.file.delete()
+    # if it has a body media, then proceed
+    if model_instance.body_media_id:
+        body_media_object = \
+            model_instance.body_media_type.model_class().objects.get(
+            id=model_instance.body_media_id)
+        body_media_object.delete()
+
+    # print(f'\n\n\nEXECUTED SIGNAL:  review deleted\n\n\n')
